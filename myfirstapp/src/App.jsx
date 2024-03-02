3dc5c5b87b149abe6bccdc876c2fcadda0e3e50a
massimiliano-demiri <massimilianodemiridev@proton.me>
2024-03-02 21:29:44 +0100

    props1

--- myfirstapp/src/App.jsx
+++ myfirstapp/src/App.jsx
@@ -1,10 +1,12 @@
 import { Hello } from "./Hello"
+import { Welcome } from "./Welcome"
 
 function App() {
 
   return (
     <div>
       <Hello />
+      <Welcome name="Luca"/>
     </div>
   )
 }
---
+++ myfirstapp/src/Welcome.jsx
@@ -0,0 +1,5 @@
+export function Welcome({name = "Giovanni"}){
+return(
+    <p>Welcome, {name}!</p>
+)
+}
