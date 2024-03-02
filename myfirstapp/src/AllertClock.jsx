644eb9e0f92e7fc30f5dc63833c1016255d94cc8
massimiliano-demiri <massimilianodemiridev@proton.me>
2024-03-02 21:52:10 +0100

    events

---
+++ myfirstapp/src/AllertClock.jsx
@@ -0,0 +1,6 @@
+export function AllertClock({handlerFunction}){
+   
+    return(
+        <button onClick={handlerFunction}></button>
+    )
+}
--- myfirstapp/src/App.jsx
+++ myfirstapp/src/App.jsx
@@ -1,12 +1,22 @@
+import { AllertClock } from "./AllertClock"
 import { Hello } from "./Hello"
 import { Welcome } from "./Welcome"
 
+
 function App() {
+  function handleButtonClick(){
+    const currentTime = new Date()
+    const currentHour = currentTime.getHours();
+    const currentMinute = currentTime.getMinutes();
+    const currentSecond = currentTime.getSeconds();
+    alert(`Current time is: ${currentHour}:${currentMinute}:${currentSecond}`)
+}
 
   return (
     <div>
       <Hello />
       <Welcome name="Luca" age={15}/>
+      <AllertClock handlerFunction={handleButtonClick} />
     </div>
   )
 }
