7e311954cfc1e043c407984c16873288b8c0da09
massimiliano-demiri <massimilianodemiridev@proton.me>
2024-03-03 07:19:29 +0100

    refs3

--- myfirstapp/src/Counter.jsx
+++ myfirstapp/src/Counter.jsx
@@ -1,11 +1,25 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { CounterDisplay } from "./CounterDisplay";
 
 export function Counter({ defaultValue, inc }) {
   const [counter, setCounter] = useState(defaultValue);
+  const counterChangeRef = useRef({value: counter, direction: null})
+
 useEffect(() =>{
-    console.log(counter)
+  if (counterChangeRef.current.value < counter){
+    counterChangeRef.current.direction = "up"
+  } else if (counterChangeRef.current.value > counter){
+    counterChangeRef.current.direction ="down"
+  }
+counterChangeRef.current.value = counter;
 }, [counter])
+
+useEffect(() =>{
+  if (counterChangeRef.current.value !== null){
+    console.log(counterChangeRef.current.direction)
+  }
+}, [counter])
+
   function handleButtonClickIncrement() {
     setCounter(counter + inc);
   }
